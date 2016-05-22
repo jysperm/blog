c785e6bc0b60d8750c550037a82b1f76f4108de5
jysperm <jysperm@gmail.com>
2016-05-22 23:14:12 +0800

    Display reviews as the end of posts

--- scripts/post-appends.js
+++ scripts/post-appends.js
@@ -1,7 +1,21 @@
 hexo.extend.filter.register('before_post_render', function(post) {
-  post.appends = {
+  post.appends = [{
     markdown: '## 撰写评论\n精子写了这么多年博客，收到的优秀评论少之又少，在这个属于 SNS 的时代也并不缺少向作者反馈的渠道。' +
-          '因此如果你希望撰写评论，请发邮件至 [jysperm@gmail.com](mailto:jysperm@gmail.com?subject=' +
-          encodeURIComponent('评论：' + post.title) + ') 并注明文章标题，我会挑选对读者有价值的评论附加到文章末尾。'
-  };
+      '因此如果你希望撰写评论，请发邮件至 [jysperm@gmail.com](mailto:jysperm@gmail.com?subject=' +
+      encodeURIComponent('评论：' + post.title) + ') 并注明文章标题，我会挑选对读者有价值的评论附加到文章末尾。'
+  }];
+
+  if (post.reviews) {
+    post.appends.push({
+      markdown: '## 评论 \n' + post.reviews.map(function(review) {
+        var body = review.body, author = review.author;
+
+        if (author) {
+          return '**' + author + '** ：' + body;
+        } else {
+          return body;
+        }
+      }).join('\n- - - -\n')
+    });
+  }
 });

7cf75e5f8b2fc93e02ce1733c1a9f1794ee176e7
jysperm <jysperm@gmail.com>
2016-11-10 18:12:36 +0800

    Use h3 instead of h2 in comments block

--- scripts/post-appends.js
+++ scripts/post-appends.js
@@ -1,6 +1,6 @@
 hexo.extend.filter.register('before_post_render', function(post) {
   post.appends = [{
-    markdown: '## 撰写评论\n精子写了这么多年博客，收到的优秀评论少之又少，在这个属于 SNS 的时代也并不缺少向作者反馈的渠道。' +
+    markdown: '### 撰写评论\n精子写了这么多年博客，收到的优秀评论少之又少，在这个属于 SNS 的时代也并不缺少向作者反馈的渠道。' +
       '因此如果你希望撰写评论，请发邮件至 [jysperm@gmail.com](mailto:jysperm@gmail.com?subject=' +
       encodeURIComponent('评论：' + post.title) + ') 并注明文章标题，我会挑选对读者有价值的评论附加到文章末尾。'
   }];

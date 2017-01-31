9a3ccfe4ee3a882d246116be18d8e37086ab7fe4
jysperm <jysperm@gmail.com>
2017-01-31 17:06:53 +0800

    :sparkles: Add footprints page

---
+++ scripts/footprints.js
@@ -0,0 +1,23 @@
+const _ = require('lodash');
+const moment = require('moment');
+
+hexo.extend.tag.register('footprints', function(args, content) {
+  const {footprints} = hexo.locals.get('data');
+
+  return _.map(footprints, (days, city) => {
+    return `
+      <h3>${city}</h3>
+      <ul>
+      ${days.map( day => {
+        if (_.isDate(day)) {
+          return moment(day).format('YYYY-MM-DD');
+        } else {
+          return day;
+        }
+      }).map(day => {
+        return `<li>${day}</li>`
+      }).join('')}
+      </ul>
+    `;
+  }).join('');
+});
---
+++ source/footprints.md
@@ -0,0 +1,7 @@
+---
+title: 足迹
+permalink: footprints/
+layout: footprints
+---
+
+{% footprints %}

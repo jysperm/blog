6a1e4fbe6a4f67eef923436a02ead4bb6d720d11
jysperm <jysperm@gmail.com>
2021-01-15 02:13:54 +0800

    :hammer: Update proxy server

--- proxy/server.js
+++ proxy/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const proxy = require('express-http-proxy');
 
 const app = express();
 
@@ -13,10 +12,10 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(express.static('public/jysperm.me'));
+app.use(express.static('public'));
 
 app.use( (req, res, next) => {
-  res.status(404).sendFile(`${__dirname}/public/${req.matchedDomain}/404/index.html`);
+  res.status(404).sendFile(`${__dirname}/public/404/index.html`);
 });
 
 app.listen(process.env.LEANCLOUD_APP_PORT || 3000);
